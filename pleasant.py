b405a1989f374a4d819126de807858d888a4cd92
Jake Kaufman <theevocater@gmail.com>
2018-08-06 23:16:47 -0400

    Add words that are one handed

--- pleasant.py
+++ pleasant.py
@@ -28,8 +28,22 @@ def left_right() -> List[str]:
     return works
 
 
-def one_hand():
-    pass
+def one_hand() -> str:
+    works = []
+    for i in open('/usr/share/dict/words'):
+        word = i.strip()
+        if word[0] in LEFTHAND:
+            hand = LEFTHAND
+        else:
+            hand = RIGHTHAND
+        found = True
+        for letter in word:
+            if letter not in hand:
+                found = False
+                break
+        if found:
+            works.append(word)
+    return works
 
 
 def print_words(words: List[str]) -> None:
@@ -41,3 +55,4 @@ def print_words(words: List[str]) -> None:
 
 if __name__ == '__main__':
     print_words(left_right())
+    print_words(one_hand())
